cad1222c58ad130a3078d33f9e6625b0d60f30c2
abehan7 <hanjk13262@gmail.com>
2022-06-18 20:17:25 +0900

    fix:> minting block padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,9 @@ const CubeComponent = (props: CubeComponentProps) => {
         <AutoHeightImage src={props.src} />
       </CubeImg>
       <CubeDesc highlight={props.highlight}>
-        <div style={{ fontSize: theme.fontSizes.fontxs }}>{props.title}</div>
+        <div style={{ fontSize: theme.fontSizes.fontxs, padding: "0.2rem" }}>
+          {props.title}
+        </div>
         {/* <div style={{ fontSize: theme.fontSizes.fontlg }}>{props.desc}</div> */}
         <MintingBlockWrapper></MintingBlockWrapper>
       </CubeDesc>
